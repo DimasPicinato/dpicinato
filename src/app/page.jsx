90a6d82a5c4fe1408744928b731ecf9749813842
Dimas Picinato <dimasmatheus08@gmail.com>
2024-07-23 18:36:12 -0300

    notebook adicionado na tela principal

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,12 +3,23 @@ import './page.scss'
 export default function HomePage() {
   return <body className='HomePage'>
     <main>
-      <div className="header">
-        <h1>
-          Olá, sou <span className='text-gradient'>Dimas Picinato</span> <br />
-          Desenvolvedor Front-end
-        </h1>
-      </div>
+      <section>
+        <div className="header">
+          <h1>
+            Olá, sou <span className='text-gradient'>Dimas Picinato</span> <br />
+            Desenvolvedor <span className='text-gradient'>Front-end</span>
+          </h1>
+          <div className="description">
+          </div>
+        </div>
+        <div className="siteExemplo">
+          <img src="./macbook-molde.png" alt="moldura macbook" />
+          <div className="content">
+            <p>
+            </p>
+          </div>
+        </div>
+      </section>
     </main>
   </body>
 }
